b8cdbddf6c93bbc44ff5ee13e7ca859bbc195d67
Nathan <natedaage@yahoo.com>
2017-10-03 14:16:26 -0600

    created game, again.  going better this time.  width and height are right.

---
+++ src/com/udemy/blockbreaker/Animate.java
@@ -0,0 +1,22 @@
+package com.udemy.blockbreaker;
+
+public class Animate implements Runnable {
+  
+  BlockBreakerPanel bp;
+  
+  Animate(BlockBreakerPanel b){
+    bp = b;
+  }
+
+  public void run() {
+    while(true){
+      bp.update();
+      try {
+        Thread.sleep(10);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+      
+    }
+  }
+}
